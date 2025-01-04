71a34472c5f9b7ea32e20818d4b67317eb49055b
Aniket Kulkarni <aniket.kulkarni1605@gmail.com>
2025-01-04 13:52:42 +0530

    Added utils.py and some minor changes

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,12 +12,11 @@ from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('D:\Data Science Projects\MLProject\artifacts',"train.csv")
-    test_data_path: str=os.path.join('D:\Data Science Projects\MLProject\artifacts',"test.csv")
-    raw_data_path: str=os.path.join('D:\Data Science Projects\MLProject\artifacts',"data.csv")
+    train_data_path: str=os.path.join('D:/Data Science Projects/MLProject/artifacts',"train.csv")
+    test_data_path: str=os.path.join('D:/Data Science Projects/MLProject/artifacts',"test.csv")
+    raw_data_path: str=os.path.join('D:/Data Science Projects/MLProject/artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -40,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
